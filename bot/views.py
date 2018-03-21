142b074dbf0bb403daf722b61545994c1c86a677
masataka <ar.tec.masa@gmail.com>
2018-03-22 00:19:47 +0900

    fix

--- bot/views.py
+++ bot/views.py
@@ -2,6 +2,16 @@
 import json
 import random
 import requests
+import base64
+import hashlib
+import hmac
+
+channel_secret = "593f3ff1e6a03ca15b36d50d8d56af49" # Channel secret string
+#body = ... # Request body string
+hash = hmac.new(channel_secret.encode('utf-8'),
+    body.encode('utf-8'), hashlib.sha256).digest()
+signature = base64.b64encode(hash)
+# Compare X-Line-Signature request header and the signature
 
 from django.shortcuts import render
 from django.http import HttpResponse
